9e63376c6d4d65278b5bd53928e4ccfb3f6ab1f7
Jan Wozniak <jwozniak@redhat.com>
2018-04-24 15:08:16 +0200

    easier configurability

--- generate_graph.sh
+++ generate_graph.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-export JOB_NAME=test_branch_origin_extended_builds 
-export BUILD_ID=439 
+export JENKINS=${JENKINS:-https://ci.openshift.redhat.com/jenkins}
+#export JOB_NAME=test_branch_origin_extended_builds 
+export JOB_NAME=test_branch_origin_extended_image_ecosystem
+export BUILD_ID=$(curl -s $JENKINS/job/$JOB_NAME/api/json | jq '.lastSuccessfulBuild.number')
 ./run.sh
-echo Generating graph from out_${BUILD_ID}-${JOB_NAME}/stats.json to graph_$JOB_NAME.html
-go run graph.go -i outs/${BUILD_ID}-${JOB_NAME}/stats.json -o graph_$JOB_NAME.html
+echo Generating graph from out/${BUILD_ID}-${JOB_NAME}/stats.json to graph.html
+go run graph.go -i outs/${BUILD_ID}-${JOB_NAME}/stats.json -o graph.html
